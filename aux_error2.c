d7c374f960bcdd4c4f2db954b5ab038151f775d6
tbir8256 <tbir8256@gmail.com>
2022-08-11 21:02:48 -0700

    aux_error2

--- aux_error2.c
+++ aux_error2.c
@@ -38,7 +38,6 @@ char *error_env(data_shell *datash)
 /**
  * error_path_126 - error message for path and failure denied permission.
  * @datash: data relevant (counter, arguments).
- *
  * Return: The error string.
  */
 char *error_path_126(data_shell *datash)
